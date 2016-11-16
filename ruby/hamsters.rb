3de1fff65c3a92f54edc4de96e43f3cbf4c22727
Matthew Eller <mattcjeller@gmail.com>
2016-11-15 17:58:25 -0800

    Update hamsters.rb

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,35 +1,39 @@
 puts "What is your hamster's name?"
-
-name = gets.chomp!
+name = gets.chomp
 
 puts "On a scale of 1-10, how loud is your hamster?"
-
-volume =  gets.chomp!.to_i
+volume = gets.chomp.to_i
+	while volume == 0
+		puts "Please enter a number from 1-10!"
+		volume = gets.chomp.to_i
+	end
 
 puts "What is the color of your hamster?"
-color = gets.chomp!
-
+color = gets.chomp
 
 puts "Is your your hamster suitable for adoption (y/n)?"
+suitability = gets.chomp
 
-suitability = gets.chomp!
+puts "What is the age of your hamster? Leave blank if unsure."
+age = gets.chomp
+	if age == ""
+		age = nil
+	else
+		age = age.to_i
+	end
 
-puts "What is the estimated age in years of your hamster?"
-
-age = gets.chomp!
-if age == ""
-  age = nil
-else 
-  age.to_i
-end
-puts " #{name}'s Information"
-puts "Name: #{name}"
-puts "Volume Level: #{volume}"
-puts "Color: #{color}"
-puts " Suitability for Adoption (Y/N) #{suitability}" 
+puts ""
+puts "Here's #{name}'s Information:"
+puts ""
+puts "(====================================================)"
+puts "Name:.................................. #{name}"
+puts "Volume Level:.......................... #{volume}"
+puts "Color:................................. #{color}"
+puts "Suitability for Adoption (y/n):........ #{suitability}" 
 
 if age == nil
-  puts "Age: Unknown"
+  puts "Age:................................... Unknown"
 else 
-  puts "Age: #{age}"
+  puts "Age:................................... #{age}"
 end
+puts "(====================================================)"
