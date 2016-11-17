e43e9986e84319cdc361bf4a072dd349523c13a1
Matthew Eller <mattcjeller@gmail.com>
2016-11-17 09:53:46 -0800

    Added interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,7 +3,9 @@ def encrypt(string)
   i = 0
   while i < string.length
     if string[i] == " "
-      next
+      result += " "
+    elsif string[i] == "z"
+      result += "a"
     else
       result += string[i].next
     end
@@ -17,8 +19,32 @@ def decrypt(string)
   result = ""
   i = 0
   while i < string.length
+    if string[i] == " "
+      result += " "
+    else
     result += alphabet[(alphabet.index(string[i])) - 1]
+    end
   i += 1
   end
   result
+end
+
+puts "Would you like to decrypt or encrypt a password? (decrypt / encrypt)"
+response = gets.chomp
+
+until response == "decrypt" || response == "encrypt"
+  puts "Please enter a valid response"
+  puts ""
+  puts "Would you like to decrypt or encrypt a password? (decrypt / encrypt)"
+  response = gets.chomp
+end
+
+puts "Please enter password"
+password = gets.chomp
+
+puts "Here is your result:"
+if response == "decrypt"
+  puts decrypt(password)
+elsif response == "encrypt"
+  puts encrypt(password)
 end
