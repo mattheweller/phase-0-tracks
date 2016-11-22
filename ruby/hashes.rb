dc35b3440ba6f704747f74e01101100ddb73ce52
Matthew Eller <mattcjeller@gmail.com>
2016-11-21 16:42:25 -0800

    more edits to program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -33,15 +33,6 @@
 
 # 10. Print the latest version of the hash, and offer to enter another client or exit the program.
 
-def p_hash(hash)
-  puts "Thank you for your entry. You entered:"
-  puts "(====================================)"
-  hash.each do |key, value|
-    puts "#{key}: #{value}"
-  end
-  puts "(====================================)"
-end
-
 hash = {
   Name: "",
   Phone: "",
@@ -53,63 +44,80 @@ hash = {
   "Favorite Band" => ""
 }
 
-puts "Please enter the following information:"
-puts "Please enter your name:"
-hash[:Name] = gets.chomp
+another, client_directory = "", []
 
-puts "Please enter your phone:"
-hash[:Phone] = gets.chomp
-
-puts "Please enter your email:"
-hash[:Email] = gets.chomp
-
-puts "Would you like to subscribe to our newsletter? (y / n)"
-if gets.chomp == "y"
-  hash[:Subscribed] = true
-else
-  hash[:Subscribed] = false
-end
- 
-puts "Please enter your age:"
-hash[:Age] = gets.chomp.to_i
-
-puts "Please enter the number of children you have:"
-hash["Number of Children"] = gets.chomp.to_i
-
-puts "Please enter a decor theme:"
-hash["Decor Theme"] = gets.chomp
-
-puts "Please enter the name of your favorite 90's band:"
-hash["Favorite Band"] = gets.chomp
-
-p_hash(hash)
-puts ""
-
-puts "Please confirm that this information is correct. If you need to edit a field, please enter the name of that field. Otherwise, simply enter 'none':"
-input = gets.chomp
-
-if input == "none"
-  p_hash(hash)
-else
-
-  puts "Ok. You'd like to change the #{input} field, correct? (y /n)"
-  response = gets.chomp
-  if response == "y"
-    puts "Please enter your update to the #{input} field"
-    edit = gets.chomp
-    if hash.has_key?(input.to_sym) && hash[input.to_sym].class == Fixnum
-      hash[input.to_sym] = edit.to_i
-    elsif hash.has_key?(input.to_sym)
-      hash[input.to_sym] = edit
-    elsif hash.has_key?(input) && hash[input].class == Fixnum
-      hash[input] = edit.to_i
-    elsif hash.has_key?(input)
-      hash[input] = edit
-    end
-    
-  p_hash(hash)
-  
-  elsif response == "n"
-    p_hash(hash)
+def p_hash(hash)
+  puts "Thank you for your entry. You entered:"
+  puts "(====================================)"
+  hash.each do |key, value|
+    puts "#{key}: #{value}"
   end
+  puts "(====================================)"
+end
+
+until another == "n"
+  puts "Please enter the following information:"
+  puts "Please enter your name:"
+  hash[:Name] = gets.chomp
+  
+  puts "Please enter your phone:"
+  hash[:Phone] = gets.chomp
+  
+  puts "Please enter your email:"
+  hash[:Email] = gets.chomp
+  
+  puts "Would you like to subcribe to our newsletter? (y / n)"
+  if gets.chomp == "y"
+    hash[:Subscribed] = true
+  else
+    hash[:Subscribed] = false
+  end
+   
+  puts "Please enter your age:"
+  hash[:Age] = gets.chomp.to_i
+  
+  puts "Please enter the number of children you have:"
+  hash["Number of Children"] = gets.chomp.to_i
+  
+  puts "Please enter a decor theme:"
+  hash["Decor Theme"] = gets.chomp
+  
+  puts "Please enter the name of your favorite 90's band:"
+  hash["Favorite Band"] = gets.chomp
+  
+  p_hash(hash)
+  puts ""
+  
+  puts "Please confirm that this information is correct. If you need to edit a field, please enter the name of that field. Otherwise, simply enter 'none':"
+  input = gets.chomp
+  
+  if input == "none"
+    p_hash(hash)
+  else
+  
+    puts "Ok. You'd like to change the #{input} field, correct? (y /n)"
+    response = gets.chomp
+    if response == "y"
+      puts "Please enter your update to the #{input} field"
+      edit = gets.chomp
+      if hash.has_key?(input.to_sym) && hash[input.to_sym].class == Fixnum
+        hash[input.to_sym] = edit.to_i
+      elsif hash.has_key?(input.to_sym)
+        hash[input.to_sym] = edit
+      elsif hash.has_key?(input) && hash[input].class == Fixnum
+        hash[input] = edit.to_i
+      elsif hash.has_key?(input)
+        hash[input] = edit
+      end
+      
+    p_hash(hash)
+    
+    elsif response == "n"
+      p_hash(hash)
+    end
+  end
+  
+  client_directory << hash
+  puts "Would you like to enter another client? (y / n)"
+  another = gets.chomp
 end
