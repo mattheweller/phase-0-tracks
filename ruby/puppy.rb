ba5d50b844f6e184b58cf49d719aefa70f5aecb7
Matthew Eller <mattcjeller@gmail.com>
2016-11-28 13:37:00 -0800

    added puppy characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+SPECIES ------------------------
+Canis lupus familiaris
+
+CHARACTERISTICS ----------------
+Tail length: varies
+Ear length: varies
+Tounge length: varies
+Coat length: varies
+Coat type: varies
+Friendly: yes
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Fetch
+Run
+Sit
+Rollover
+Play dead
