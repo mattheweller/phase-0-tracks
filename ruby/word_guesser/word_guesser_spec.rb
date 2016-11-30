4da06d96fdb1a43f3127d925c6c7c63dfe8cf4d8
Matthew Eller <mattcjeller@gmail.com>
2016-11-30 14:29:18 -0800

    created files and directories. Pseudocode in progress

---
+++ ruby/word_guesser/word_guesser_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'word_guesser'
+
+describe WordGusser do
+  let(:word_guesser) { WordGusser.new }
+
+  it "" do
+    #expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "" do
+    #expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "" do
+    #expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "" do
+    #expect(calculator.add(3,4)).to eq 7
+  end
+
+end
