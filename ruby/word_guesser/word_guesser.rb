3e38ba07ec1573a2cf63e2188cf9aac91580651a
Matthew Eller <mattcjeller@gmail.com>
2016-11-30 23:10:58 -0800

    added and passed tests

--- ruby/word_guesser/word_guesser.rb
+++ ruby/word_guesser/word_guesser.rb
@@ -22,58 +22,35 @@ class WordGuesser
 end
 
 # UI
+# commented out to test the class with rspec
 
-puts "Welcome to the Word Guessing game!"
-puts "Player 1, enter your Secret Word!"
-word = gets.chomp
-game = WordGuesser.new(word)
-status = game.dasherize
-
-puts "Secret Word: #{status.join(" ")}"
-
-while !game.is_over
-  puts "Player 2, you have #{game.guess_count} guesses left! Guess a letter!"
-  letter = gets.chomp
-  game.secret_word.each_with_index do |secret_letter, dash|
-    status[dash] = letter if secret_letter == letter
-  end
-
-  puts "Secret Word: #{status.join(" ")}"
-  game.guess_count -= 1
-
-  unless game.guesses.include?(letter) || !game.check_guess(letter)
-    game.guesses << letter
-  end
-
-  if game.secret_word.uniq.sort == game.guesses.uniq.sort
-    game.is_over = true
-    puts "Player 2 Wins!! :D"
-  elsif game.guess_count == 0
-    puts "Player 2 Loses!! :P"
-    break
-  end
-end
-
-
-
-
-
-#notes
-# guess = "g"
-# guess_array = ["g", "a", "m", "e"]
-# guess_array.include?(guess) #=> true
-
-
-# word = "gamea".split("")
-# display = "12345".split("")
-# guess = "a"
-# word.each_with_index do |ele, i|
-#   display[i] = guess if ele == guess
-# end
-# p display.join(" ")
-
-
-# one = "qaaweerty".split("")
-# two = "ytrewaaaq".split("")
+# puts "Welcome to the Word Guessing game!"
+# puts "Player 1, enter your Secret Word!"
+# word = gets.chomp
+# game = WordGuesser.new(word)
+# status = game.dasherize
 #
-# one.uniq.sort == two.uniq.sort
+# puts "Secret Word: #{status.join(" ")}"
+#
+# while !game.is_over
+#   puts "Player 2, you have #{game.guess_count} guesses left! Guess a letter!"
+#   letter = gets.chomp
+#   game.secret_word.each_with_index do |secret_letter, dash|
+#     status[dash] = letter if secret_letter == letter
+#   end
+#
+#   puts "Secret Word: #{status.join(" ")}"
+#   game.guess_count -= 1
+#
+#   unless game.guesses.include?(letter) || !game.check_guess(letter)
+#     game.guesses << letter
+#   end
+#
+#   if game.secret_word.uniq.sort == game.guesses.uniq.sort
+#     game.is_over = true
+#     puts "Player 2 Wins!! :D"
+#   elsif game.guess_count == 0
+#     puts "Player 2 Loses!! :P"
+#     break
+#   end
+# end
--- ruby/word_guesser/word_guesser_spec.rb
+++ ruby/word_guesser/word_guesser_spec.rb
@@ -3,20 +3,31 @@ require_relative 'word_guesser'
 describe WordGuesser do
   let(:word_guesser) { WordGuesser.new("game") }
 
+  it "stores the secret word as an array" do
+    expect(word_guesser.secret_word).to eq ["g", "a", "m", "e"]
+  end
+
+  it "stores the guesses in an array" do
+    expect(word_guesser.guesses).to eq []
+  end
+
+  it "Gives you 3 more guesses than the length of the secret word" do
+    expect(word_guesser.guess_count).to eq 7
+  end
+
+  it "initializes is_over as false" do
+    expect(word_guesser.is_over).to eq false
+  end
+
   it "Checks guess and returns a boolean" do
     expect(word_guesser.check_guess("g")).to eq true
   end
 
-  # it "stores the secret word" do
-  #   expect(word_guesser.secret_word).to eq "game"
-  # end
-  #
-  # it "" do
-  #   #expect(word_guesser.guess("g")).to eq "g"
-  # end
-  #
-  # it "" do
-  #   #expect(word_guesser.guess("g")).to eq "g"
-  # end
+  it "Checks guess and returns a boolean" do
+    expect(word_guesser.check_guess("q")).to eq false
+  end
 
+  it "Converts the secret word into space separated underscores" do
+    expect(word_guesser.dasherize).to eq ["_", "_", "_", "_"]
+  end
 end
