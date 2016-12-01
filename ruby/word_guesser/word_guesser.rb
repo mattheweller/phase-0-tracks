6a1231f8ba11ce0781e604f285456051c9bde7d9
Matthew Eller <mattcjeller@gmail.com>
2016-11-30 20:45:46 -0800

    coded working version of the game

--- ruby/word_guesser/word_guesser.rb
+++ ruby/word_guesser/word_guesser.rb
@@ -1,18 +1,68 @@
 # game class
 
-class WordGusser
+class WordGuesser
+  attr_accessor :secret_word, :guesses, :guess_count, :is_over
 
+  def initialize(secret_word)
+    @secret_word = secret_word.split("")
+    @guesses = []
+    @guess_count = (@secret_word.length + 3)
+    @is_over = false
+  end
+
+  def check_guess(guess)
+    @secret_word.include?(guess)
+  end
+
+  def dasherize
+    @secret_word.map do |letter|
+      letter = "_"
+    end
+  end
 end
 
 # UI
+
+puts "Welcome to the Word Guessing game!"
+puts "Player 1, enter your Secret Word!"
+word = gets.chomp
+game = WordGuesser.new(word)
+status = game.dasherize
+
+puts "Secret Word: #{status.join(" ")}"
+
+while !game.is_over
+  puts "Player 2, you have #{game.guess_count} guesses left! Guess a letter!"
+  letter = gets.chomp
+  game.secret_word.each_with_index do |secret_letter, dash|
+    status[dash] = letter if secret_letter == letter
+  end
+
+  puts "Secret Word: #{status.join(" ")}"
+  game.guess_count -= 1
+
+  unless game.guesses.include?(letter) || !game.check_guess(letter)
+    game.guesses << letter
+  end
+
+  if game.secret_word.uniq.sort == game.guesses.uniq.sort
+    game.is_over = true
+    puts "Player 2 Wins!! :D"
+  elsif game.guess_count == 0
+    puts "Player 2 Loses!! :P"
+    break
+  end
+end
+
+
 
 
 
 #notes
 # guess = "g"
-# guess_array = ["g"]
-
+# guess_array = ["g", "a", "m", "e"]
 # guess_array.include?(guess) #=> true
+
 
 # word = "gamea".split("")
 # display = "12345".split("")
--- ruby/word_guesser/word_guesser_spec.rb
+++ ruby/word_guesser/word_guesser_spec.rb
@@ -1,31 +1,22 @@
 require_relative 'word_guesser'
 
-describe WordGusser do
-  let(:word_guesser) { WordGusser.new("game") }
+describe WordGuesser do
+  let(:word_guesser) { WordGuesser.new("game") }
 
-  it "stores a guess" do
-    expect(word_guesser.guess_letter_at(0, "g")).to eq "g"
+  it "Checks guess and returns a boolean" do
+    expect(word_guesser.check_guess("g")).to eq true
   end
 
-  it "stores the secret word" do
-    expect(word_guesser.secret_word).to eq "game"
-  end
-
-  it "" do
-    #expect(word_guesser.guess("g")).to eq "g"
-  end
-
-  it "" do
-    #expect(word_guesser.guess("g")).to eq "g"
-  end
+  # it "stores the secret word" do
+  #   expect(word_guesser.secret_word).to eq "game"
+  # end
+  #
+  # it "" do
+  #   #expect(word_guesser.guess("g")).to eq "g"
+  # end
+  #
+  # it "" do
+  #   #expect(word_guesser.guess("g")).to eq "g"
+  # end
 
 end
-
-
-#tests:
-guess counter
-guess counter doesn't increment when already guessed letter guessed
-display equals array.length of secret word
-display update returns
-Guesses left counts down to one, then prints taunting message
-correct guess flips the value of is_over, prints the winning message, showing the secret word and exits the game
