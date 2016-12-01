ada542ba48d1aefc4157c3114b8863623a541ae4
Matthew Eller <mattcjeller@gmail.com>
2016-11-30 16:10:40 -0800

    Working on psuedocode

--- ruby/word_guesser/word_guesser.rb
+++ ruby/word_guesser/word_guesser.rb
@@ -5,3 +5,19 @@ class WordGusser
 end
 
 # UI
+
+
+
+#notes
+# guess = "g"
+# guess_array = ["g"]
+
+# guess_array.include?(guess) #=> true
+
+# word = "gamea".split("")
+# display = "12345".split("")
+# guess = "a"
+# word.each_with_index do |ele, i|
+#   display[i] = guess if ele == guess
+# end
+# p display.join(" ")
