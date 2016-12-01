37230c0d9856ed8c4581ea55f80e131f452be747
Matthew Eller <mattcjeller@gmail.com>
2016-11-30 23:23:29 -0800

    refactored minor changes in word guesser

--- ruby/word_guesser/word_guesser.rb
+++ ruby/word_guesser/word_guesser.rb
@@ -22,35 +22,32 @@ class WordGuesser
 end
 
 # UI
-# commented out to test the class with rspec
 
-# puts "Welcome to the Word Guessing game!"
-# puts "Player 1, enter your Secret Word!"
-# word = gets.chomp
-# game = WordGuesser.new(word)
-# status = game.dasherize
-#
-# puts "Secret Word: #{status.join(" ")}"
-#
-# while !game.is_over
-#   puts "Player 2, you have #{game.guess_count} guesses left! Guess a letter!"
-#   letter = gets.chomp
-#   game.secret_word.each_with_index do |secret_letter, dash|
-#     status[dash] = letter if secret_letter == letter
-#   end
-#
-#   puts "Secret Word: #{status.join(" ")}"
-#   game.guess_count -= 1
-#
-#   unless game.guesses.include?(letter) || !game.check_guess(letter)
-#     game.guesses << letter
-#   end
-#
-#   if game.secret_word.uniq.sort == game.guesses.uniq.sort
-#     game.is_over = true
-#     puts "Player 2 Wins!! :D"
-#   elsif game.guess_count == 0
-#     puts "Player 2 Loses!! :P"
-#     break
-#   end
-# end
+puts "Welcome to the Word Guessing game!"
+puts "Player 1, enter your Secret Word!"
+word = gets.chomp
+game = WordGuesser.new(word)
+status = game.dasherize
+
+until game.is_over
+  puts "Player 2, you have #{game.guess_count} guesses left! Guess a letter!"
+  puts "Secret Word: #{status.join(" ")}"
+  letter = gets.chomp
+  game.secret_word.each_with_index do |secret_letter, dash|
+    status[dash] = letter if secret_letter == letter
+  end
+
+  game.guess_count -= 1
+
+  unless game.guesses.include?(letter) || !game.check_guess(letter)
+    game.guesses << letter
+  end
+
+  if game.secret_word.uniq.sort == game.guesses.uniq.sort
+    game.is_over = true
+    puts "Player 2 Wins!! :D"
+  elsif game.guess_count == 0
+    puts "Player 2 Loses!! :P"
+    break
+  end
+end
