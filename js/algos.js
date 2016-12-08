54a711264b197544dfa3db584aca2d9001406cb1
Matthew Eller <mattcjeller@gmail.com>
2016-12-07 16:00:20 -0800

    began R1 before R0

---
+++ js/algos.js
@@ -0,0 +1,19 @@
+// function takes two objects
+// check to see if the objects share at least one key-value pair.
+// If called with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, return true
+// If no pairs match, return false.
+// Hint: String ('age') or an identifier name (age) can be considered equivalent.
+
+// Add some driver code that tests both outcomes of your function.
+
+// 1. Declare a function that takes two objects.
+// 2. Created a nested loop to check each property for equivalency.
+// 3. If an equivalency is found, return true, else return false.
+// 4. Create test data, and use the function to call the function.
+
+function pairMatch(obj1, obj2) {
+  for (var i = 0; i < obj1.length; i++) {
+
+  }
+
+}
