b1ebf0f3d54535e6815dd08236474bdc2b0efec9
Matthew Eller <mattcjeller@gmail.com>
2016-12-07 18:30:17 -0800

    Completed R2 and wrote tests to get desired return value

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,10 @@
 // RELEASE 0:
-
 // function that takes an array of words or phrases and
 // returns the longest word or phrase in the array.
 // Hint: "some string".length will do what you expect in JS.
-
 // Add driver code that tests your function on a few arrays.
 
-// PSEUDOCODE:
+// R0 PSEUDOCODE:
 // 1. Declare a function that takes an array of strings
 // 2. Inside the function, declare a variable to store the longest word
 // 3. Assign the longest word variable the value of the 0th index of the array.
@@ -16,21 +14,15 @@
 // 7. Complete the loop and, reassigning as many times as necessary, and return the longest word.
 
 function longestWord(array) {
-  var longest = array[0]
+  var longest = array[0];
 
   for (var i = 0; i < array.length; i++) {
     if (array[i].length > longest.length) {
-      longest = array[i]
+      longest = array[i];
     }
   }
   return longest;
 }
-
-var dinner = ["spaghetti","meat balls","pepperoni pizza"];
-var phrases = ["what would you like for dinner?","I'm not sure I'm that hungry yet","Check please!"];
-
-console.log("The longest word is" + " '" + longestWord(dinner) + "' " + "with a length of " + longestWord(dinner).length + " letters!");
-console.log("The longest word is" + " '" + longestWord(phrases) + "' " + "with a length of " + longestWord(phrases).length + " letters!");
 
 // RELEASE 1:
 // function takes two objects
@@ -38,11 +30,9 @@ console.log("The longest word is" + " '" + longestWord(phrases) + "' " + "with a
 // If called with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, return true
 // If no pairs match, return false.
 // Hint: String ('age') or an identifier name (age) can be considered equivalent.
-
 // Add some driver code that tests both outcomes of your function.
 
-// PSEUDOCODE:
-
+// R1 PSEUDOCODE:
 // 1. Declare a function that takes two objects.
 // 2. Created a nested loop to check each property for equivalency.
   // i. Test for equivalency: Compare the key of one object against a given key of the other object,
@@ -59,7 +49,7 @@ function pairMatch(obj1, obj2) {
       }
     }
   }
-  return false
+  return false;
 }
 
 // Cleaner solution:
@@ -74,17 +64,72 @@ function pairMatch(obj1, obj2) {
   }
   return false;
 }
-  
-var obj1 = {name: "Steven", age: 54}
-var obj2 = {name: "Tamir", age: 54}
+
+// RELEASE 1:
+// function that takes an integer for length, and builds and returns an array of strings
+// of the given length. So if we ran your function with an argument of 3, we would get
+// an array of 3 random words back.
+// The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters.
+
+// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+
+// R1 PSEUDOCODE:
+// 1. Declare a function that takes a number
+// 2. Create a variable that stores a string of the alphabet as one word.
+// 3. Create an empty array
+// 4. Being a for loop, and loop the argument's number of times
+// 5. Create an empty string
+// 6. Generate a random number between 1 and 10
+// 7. enter a nested for loop, and loop for the duration of that random number.
+// 8. Create an random number the length of the alphabet string.
+// 9. add and assign a letter with the random index variable
+// 10. Build up the random word, and exit the nested for loop
+// 11. Push the built up word into the empty array.
+// 12. Repeat the whole process until exhausting the outter loop.
+// 13. return the built up array.
+
+function autoTest(number) {
+  var letters = "wertyuiopljhgfdsacbnm"
+  var result = []
+
+  for (var i = 0; i < number; i++) {
+    var randomWord = "";
+    var wordLength = (Math.floor(Math.random() * 9) + 1);
+
+    for (var j = 0; j < wordLength; j++) {
+      var letterIndex = Math.floor(Math.random() * letters.length);
+      randomWord += letters[letterIndex];
+    }
+    result.push(randomWord);
+  }
+  return result;
+}
+
+// // R0 Tests:
+var dinner = ["spaghetti","meat balls","pepperoni pizza"];
+var phrases = ["what would you like for dinner?","I'm not sure I'm that hungry yet","Check please!"];
+
+console.log("The longest word is" + " '" + longestWord(dinner) + "' " + "with a length of " + longestWord(dinner).length + " letters!");
+console.log("The longest word is" + " '" + longestWord(phrases) + "' " + "with a length of " + longestWord(phrases).length + " letters!");
+
+// //R1 Tests:
+var obj1 = {name: "Steven", age: 54};
+var obj2 = {name: "Tamir", age: 54};
 var horses = {
   "Fred"        : "red",
   "Dibbs"       : "green",
   "One Piece"   : "blue",
   "Square Deal" : "yellow",
   "Hoof Hearted": "brown"
-}
+};
 
 console.log(pairMatch(obj1, obj2)); //=> true
 console.log(pairMatch(obj1, horses)); //=> false
 console.log(pairMatch(horses, horses)); //=> true
+
+// //R2 Tests:
+for (var i = 1; i < 10; i++) {
+  var test = autoTest(i);
+  console.log(test);
+  console.log(longestWord(test));
+}
