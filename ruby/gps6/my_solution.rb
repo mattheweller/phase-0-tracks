665987b425d81ed49529b444afdfb3cdc2039120
Matthew Eller <mattcjeller@gmail.com>
2016-12-09 10:41:47 -0800

    DRY'd private methods with loops

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -9,57 +9,51 @@
 require_relative 'state_data'
 
 class VirusPredictor
-# initialize initializes an instance of the class VirusPredictor with state_of_origin, population_density and population
+
+  # initialize initializes an instance of the class VirusPredictor with state_of_origin, population_density and population
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-# Publically displays the report
+
+  # Publically displays the report
   def virus_effects
     display_report
   end
-# private secures proprietary information within the object instance
+
+  # private secures proprietary information within the object instance
   private
-# display_report prints the output of predicted_deaths and speed_of_spread
+
+  # display_report prints the output of predicted_deaths and speed_of_spread
   def display_report
     print "#{@state} will lose #{predicted_deaths} people in this outbreak"
     print " and will spread across the state in #{speed_of_spread} months.\n\n"
   end
-# predicted_deaths looks at population_density and calculates number of death and prints that output.
+
+  # predicted_deaths looks at population_density and calculates number of death and prints that output.
   def predicted_deaths
     # predicted deaths is solely based on population density
-    case
-      when @population_density >= X_LARGE_STATE[:pop_dense]
-        number_of_deaths = (@population * X_LARGE_STATE[:pop_death]).floor
-      when @population_density >= LARGE_STATE[:pop_dense]
-        number_of_deaths = (@population * LARGE_STATE[:pop_death]).floor
-      when @population_density >= MEDIUM_STATE[:pop_dense]
-        number_of_deaths = (@population * MEDIUM_STATE[:pop_death]).floor
-      when @population_density >= SMALL_STATE[:pop_dense]
-        number_of_deaths = (@population * SMALL_STATE[:pop_death]).floor
-      else
-        number_of_deaths = (@population * 0.05).floor
+    STATE_FORMULA.each do |state_size, calculation|
+      if @population_density >= calculation[:pop_density]
+        return number_of_deaths = (@population * calculation[:deaths]).floor
+      end
     end
+    number_of_deaths = (@population * 0.05).floor
   end
-# speed_of_spread looks at population_density and calculates the speed of spread of the disease.
+
+  # speed_of_spread looks at population_density and calculates the speed of spread of the disease.
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    case
-      when @population_density >= X_LARGE_STATE[:pop_dense]
-        speed += X_LARGE_STATE[:speed]
-      when @population_density >= LARGE_STATE[:pop_dense]
-        speed += LARGE_STATE[:speed]
-      when @population_density >= MEDIUM_STATE[:pop_dense]
-        speed += MEDIUM_STATE[:speed]
-      when @population_density >= SMALL_STATE[:pop_dense]
-        speed += SMALL_STATE[:speed]
-      else
-        speed += 2.5
+    STATE_FORMULA.each do |state_size, calculation|
+      if @population_density >= calculation[:pop_density]
+        return speed += calculation[:speed]
+      end
     end
+    speed += 2.5
   end
 
 end
