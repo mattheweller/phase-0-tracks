c4550af5d64a214b4bab024b03e97809715faee3
Matthew Eller <mattcjeller@gmail.com>
2016-12-16 16:06:02 -0800

    R3: Complete. Features built.

--- databases/todo_list/list_maker.rb
+++ databases/todo_list/list_maker.rb
@@ -1,13 +1,3 @@
-# List Maker
-
-# // Move this to a README.md:
-# Life is complicated and full of opportunities for things to slip through
-# the cracks.  That’s why there’s List Maker!
-# A terminal based to do list for your fairly inconvenient convenience!
-# #listmaker #fairlyinconvenientconvenience
-# talk about Object Relational Mapping (ORM)
-
-# require gems
 require 'sqlite3'
 require 'faker'
 
@@ -24,11 +14,6 @@ SQL
 
 notebook.execute(create_table_cmd)
 
-# test notes
-# notebook.execute("INSERT INTO notes (list_counter, note) VALUES (2, 'A second test note.')")
-# notebook.execute("SELECT * FROM notes").length
-# notebook.execute("INSERT INTO notes (list_counter, note) VALUES (#{counter(notebook)}, 'This is a test note.')")
-
 # CRUD Methods:
 # create
 def create_note(notebook, note)
@@ -41,7 +26,7 @@ end
 
 # read / print
 def print_list(notebook)
-  list = notebook.execute("SELECT * FROM notes")
+  list = notebook.execute("SELECT * FROM notes ORDER BY list_counter")
   list.each do |notes|
     print "Note #{notes['list_counter']}: #{notes['note']} \n"
   end
@@ -57,40 +42,49 @@ def delete_notebook(notebook)
   notebook.execute("DROP TABLE notes")
 end
 
-# def delete_note(notebook, number)
-#   notebook.execute("DELETE FROM notes WHERE list_counter=?", [number])
-#   notebook.execute("SELECT list_counter FROM notes WHERE list_counter > ?") do |row|
-#     notebook.execute("UPDATE notes SET list_counter = (list_counter - 1) WHERE list_counter >= ?", [number - 1])
-#     # notebook.execute("UPDATE notes SET list_counter=? WHERE list_counter > ?", [number, (row + number - 1)])
-#   end
-# end
-#
-# def reset_counter(notebook)
-#   counter(notebook).times do |i|
-#     .execute("SELECT * FROM notes") do |row|
-#     notebook.execute("UPDATE notes SET list_counter=#{row}")
-#   end
-# end
-
 def delete_note(notebook, number)
   notebook.execute("DELETE FROM notes WHERE list_counter = ?", number)
   notebook.execute("UPDATE notes SET list_counter = (list_counter - 1) WHERE list_counter > ?", number)
 end
 
 # UI
+puts "Welcome to List Maker!"
 
-# puts "Welcome to List Maker!"
-# puts "What would you like to do?"
-# action = gets.chomp
+done = false
+until done
+  puts ""
+  puts "Would you like to create, edit, display, or a delete a note?"
+  puts "You can also delete your whole notebook"
+  puts "if you're done with this program, just say 'done'"
+  action = gets.chomp.downcase
 
-# Tests
-
-# delete_notebook(notebook)
-# create_note(notebook, "Test note #{counter(notebook)}")
-# # # counter(notebook)
-# update(notebook, 3, "Updated test note.")
-print_list(notebook)
-delete_note(notebook, 5)
-print_list(notebook)
-# # reset_counter(notebook)
-# p notebook.execute("SELECT list_counter FROM notes")
+  case
+  when action == "create"
+    puts "Please enter your note"
+    note = gets.chomp
+    create_note(notebook, note)
+    print_list(notebook)
+  when action == "edit"
+    puts "Please enter the note number to edit"
+    number = gets.chomp.to_i
+    puts "Please enter your edits"
+    note = gets.chomp.downcase
+    update(notebook, number, note)
+    print_list(notebook)
+  when action == "display"
+    puts "Here are your notes:"
+    print_list(notebook)
+  when action == "delete"
+    puts "Please enter the note number to delete"
+    number = gets.chomp.to_i
+    delete_note(notebook, number)
+    print_list(notebook)
+  when action == "delete notebook"
+    puts "Are you sure you'd like to delete your notebook?"
+    answer = gets.chomp.downcase
+    delete_notebook(notebook) if answer == "yes"
+    puts "Your notebook has been deleted."
+  when action == "done"
+    done = true
+  end
+end
