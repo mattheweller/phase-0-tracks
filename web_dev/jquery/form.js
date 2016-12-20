83eb9a65c84f025e5abd34210aa7f419b4cd92fa
Matthew Eller <mattcjeller@gmail.com>
2016-12-19 18:41:50 -0800

    Used jQuery selectors, and actions to add items to a lists.

---
+++ web_dev/jquery/form.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  var $newItemButton = $('#newItemButton');
+  var $newItemForm = $('#newItemForm');
+  var $textInput = $('input:text');
+
+  $newItemButton.show();
+  $newItemForm.hide();
+
+  $('#showForm').on('click', function(){
+    $newItemButton.hide();
+    $newItemForm.show();
+  });
+
+  $newItemForm.on('submit', function(event){
+    event.preventDefault();
+    if (event.isDefaultPrevented()); {
+      var item = $textInput.val();
+      $('ul li:first').before('<li>' + item + '</li>');
+      $newItemForm.hide();
+      $newItemButton.show();
+      $textInput.val('');
+    }
+  });
+
+});
