9b7ae2a6d4753252e869cebb415686da173afdb3
Matthew Eller <mattcjeller@gmail.com>
2016-12-20 13:00:09 -0800

    DRYing js

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,8 +1,10 @@
 var firstHOne = document.getElementsByTagName('h1')[0];
 firstHOne.style.color = "green";
 
-var paragraph = document.getElementsByTagName('p')[0];
-paragraph.onclick = function() {
-  paragraph.style.visibility = 'hidden';
+function hideText(event) {
+  event.target.style.visibility = 'hidden';
 }
+
+var paragraph = document.getElementsByTagName('p')[0];
+paragraph.addEventListener('click', hideText)
 
