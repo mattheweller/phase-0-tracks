467c876cf69f2369ca1fbcc40e84af5ccbde3037
Matthew Eller <mattcjeller@gmail.com>
2016-12-20 10:38:29 -0800

    R0: Complete except Optional Bonus

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -9,6 +9,7 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
+  # "<h1>Hello World!</h1>"
   "#{params[:name]} is #{params[:age]} years old."
 end
 
@@ -23,8 +24,8 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
-# write a GET route that retrieves
-# all student data
+write a GET route that retrieves
+all student data
 get '/students' do
   students = db.execute("SELECT * FROM students")
   response = ""
@@ -42,5 +43,30 @@ end
 
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
+  # student.to_s
+  result = ""
+  result << "ID: #{student['id']}<br>"
+  result << "Name: #{student['name']}<br>"
+  result << "Age: #{student['age']}<br>"
+  result << "Campus: #{student['campus']}<br><br>"
+  result
+end
+
+get '/contact' do
+  "<p>400 Baker St. Apt. 104<br>
+   San Francisco, CA 94117</p>"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/:number_1/math/:number2' do
+  "#{params[:number_1]} plus #{params[:number2]} eqauls
+  #{(params[:number_1]).to_i + (params[:number_1]).to_i}"
 end
---
+++ web_dev/sinatra_intro/notes.md
@@ -0,0 +1,3 @@
+Query Parameter is a set of key value pairs that are added to a URL
+
+`<URL>/<route>?<params variable>=<value>`
